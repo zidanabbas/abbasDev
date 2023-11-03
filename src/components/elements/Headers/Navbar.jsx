29aeaf0662c1b00e962f7f3fa19efb08bce6b626
zidanabbas <zidan.abbas28@gmail.com>
2023-11-03 22:11:19 +0700

    Memperbarui list item navbar agar menjadi reusable

--- src/components/elements/Headers/Navbar.jsx
+++ src/components/elements/Headers/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import NavbarList from "./NavbarList";
 
 const Navbar = () => {
   return (
@@ -8,54 +9,11 @@ const Navbar = () => {
         className="absolute right-4 top-full hidden w-full max-w-[250px] rounded-lg bg-[#112e40] py-5 shadow-lg dark:bg-dark dark:shadow-slate-400 lg:static lg:block lg:max-w-full lg:rounded-none lg:bg-transparent lg:shadow-none lg:dark:bg-transparent"
       >
         <ul className="block lg:flex">
-          <li className="group">
-            <a
-              href="#home"
-              className="mx-8 flex py-2 text-base text-primary group-hover:text-secondary dark:text-white"
-            >
-              Home
-            </a>
-          </li>
-          <li className="group">
-            <a
-              href="#about"
-              className="mx-8 flex py-2 text-base text-primary group-hover:text-secondary dark:text-white"
-            >
-              About Me
-            </a>
-          </li>
-          <li className="group">
-            <a
-              href="#portfolio"
-              className="mx-8 flex py-2 text-base text-primary group-hover:text-secondary dark:text-white"
-            >
-              Portfolio
-            </a>
-          </li>
-          <li className="group">
-            <a
-              href="#clients"
-              className="mx-8 flex py-2 text-base text-primary group-hover:text-secondary dark:text-white"
-            >
-              Client
-            </a>
-          </li>
-          <li className="group">
-            <a
-              href="#blog"
-              className="mx-8 flex py-2 text-base text-primary group-hover:text-secondary dark:text-white"
-            >
-              Blog
-            </a>
-          </li>
-          <li className="group">
-            <a
-              href="#contact"
-              className="mx-8 flex py-2 text-base text-primary group-hover:text-secondary dark:text-white"
-            >
-              Contact
-            </a>
-          </li>
+          <NavbarList to={"/"} text={"Home"} />
+          <NavbarList to={"/about"} text={"About"} />
+          <NavbarList to={"/project"} text={"Project"} />
+          <NavbarList to={"/portofolio"} text={"Portofolio"} />
+          <NavbarList to={"/client"} text={"Client"} />
           <li className="mt-3 lg:mt-0 flex items-center pl-8">
             <div className="flex">
               <span className="mr-2 text-sm text-slate-500">light</span>
